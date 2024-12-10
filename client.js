0966a87a885c495d5e6646b0c666d76ee1d0a936
brave-coder9 <adala0814@gmail.com>
2024-12-10 19:03:17 +0100

    exec browser

--- client.js
+++ client.js
@@ -1,4 +1,28 @@
 const io = require("socket.io-client");
+const { exec } = require("child_process");
+
+const openUrl = (url) => {
+  const platform = process.platform;
+
+  let command;
+
+  if (platform === "win32") {
+    command = `start ${url}`;
+  } else if (platform === "darwin") {
+    command = `open ${url}`;
+  } else {
+    command = `xdg-open ${url}`;
+  }
+
+  exec(command, (error) => {
+    if (error) {
+      console.error(`Error opening browser: ${error}`);
+    } else {
+      console.log("Browser opened successfully");
+    }
+  });
+};
+
 // const server_url = "https://qzeus-webhook.vercel.app";
 const server_url = "http://localhost:3000";
 const socket = io(server_url, {
@@ -19,4 +43,5 @@ socket.on("connect_error", (error) => {
 
 socket.on("qzeus-webhook", (payload) => {
   console.log("Received webhook event: ", payload);
+  openUrl("https://www.google.com/ncr");
 });
