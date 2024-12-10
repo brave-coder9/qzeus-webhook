17fd442e14a0352ed1e3fda4e6a30a1ddcd8e19d
brave-coder9 <adala0814@gmail.com>
2024-12-10 18:02:42 +0100

    complete test

--- client.js
+++ client.js
@@ -1,14 +1,22 @@
 const io = require("socket.io-client");
-const socket = io("https://qzeus-webhook.vercel.app");
+// const server_url = "https://qzeus-webhook.vercel.app";
+const server_url = "http://localhost:3000";
+const socket = io(server_url, {
+  reconnect: true,
+});
 
 socket.on("connect", () => {
   console.log("Connected to the server");
 });
 
-socket.on("qzeus-webhook", (payload) => {
-  console.log("Received webhook event: ", payload);
-});
-
 socket.on("disconnect", () => {
   console.log("Disconnected from the server");
+});
+
+socket.on("connect_error", (error) => {
+  console.error("Connection error:", error);
+});
+
+socket.on("qzeus-webhook", (payload) => {
+  console.log("Received webhook event: ", payload);
 });
