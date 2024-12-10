9adb8fe70b548d39234eb1b9197ba3ac0aaddf27
lazar9z <lazar.prokovic@outlook.com>
2024-12-10 15:51:29 +0100

    code

---
+++ api/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+// Enable CORS
+app.use(cors());
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello Qzeus!");
+});
+
+// Webhook endpoint
+app.post("/qzeus/webhook", async (req, res, next) => {
+  try {
+    if (req.method === "POST") {
+      const payload = req.body;
+      // Handle the payload here
+      console.log(payload);
+
+      // Respond to the webhook request
+      res
+        .status(200)
+        .json({ message: "Webhook received successfully", payload });
+    } else {
+      res.status(405).json({ message: `Method not allowed: ${req.method}` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
